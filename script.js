284a0609364efe8d720a0b12256baa8efe932b6a
mariananogueirab <mariananbp@gmail.com>
2021-06-01 16:07:11 -0300

    Requisito 2

--- script.js
+++ script.js
@@ -8,3 +8,25 @@ const title = document.createElement('h1');
 title.id = 'title';
 title.innerText = 'Paleta de Cores';
 header.appendChild(title);
+
+// Requisito 2:
+// Div de fora:
+const divPalette = document.createElement('div');
+divPalette.id = 'color-palette';
+main.appendChild(divPalette);
+
+//Divs menores com as cores:
+function createDivColors(cor) {
+  let divColors = document.createElement('div');
+  divColors.className = 'color';
+  divColors.style.backgroundColor = cor;
+  divPalette.appendChild(divColors);
+}
+
+createDivColors('black');
+createDivColors('#B6537B');
+createDivColors('#FF2652');
+createDivColors('#FF0EAA');
+createDivColors('#FF9C1E');
+createDivColors('#FEF636');
+createDivColors('#ADFF91');
